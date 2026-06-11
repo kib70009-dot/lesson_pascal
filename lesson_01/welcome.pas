9ac8c11d4c45a1722aca4f5d89c0aa0181c76f5b
mark <mark.cheng@liao.com.tw>
2026-06-11 16:54:36 +0800

    update

--- lesson_01/welcome.pas
+++ lesson_01/welcome.pas
@@ -1,5 +1,13 @@
 program welcome;
+
+{$APPTYPE CONSOLE}
+
+var
+  StrMessage: string;
 begin
-    writeln('Welcome to Pascal programming!');
-    readln;
+  StrMessage := 'Hello, World'+ #13#10; 
+  StrMessage := StrMessage+ 'Welcome to Pascal programming!'+ #13;
+  Writeln(StrMessage);
+  // Wait until the Enter key is pressed
+  Readln;
 end.
