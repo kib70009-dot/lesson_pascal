8c7ce68a312ba2fce6b276308565da2e1032e311
kib70009-dot <kib70009@gmail.com>
2026-06-18 21:28:53 +0800

    add_Gui

---
+++ lesson_01/backup/main.pas
@@ -0,0 +1,41 @@
+unit Main;
+
+{$mode objfpc}{$H+}
+
+interface
+
+uses
+  Classes, SysUtils, Forms, Controls, Graphics, Dialogs, StdCtrls;
+
+type
+
+  { TForm1 }
+
+  TForm1 = class(TForm)
+    Button1: TButton;
+    procedure Button1Click(Sender: TObject);
+  private
+
+  public
+
+  end;
+
+var
+  Form1: TForm1;
+
+implementation
+
+{$R *.lfm}
+
+{ TForm1 }
+
+procedure TForm1.Button1Click(Sender: TObject);
+var sStr: string;
+begin
+  sStr:= 'Hello World!' + #13#10;
+  sStr+= 'Welcome to then pascal program!!';
+  ShowMessage(sStr);
+end;
+
+end.
+
